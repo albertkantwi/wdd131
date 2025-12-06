0eab3d7fbf79b847fc0b2a7e79f4ae2cb52891c5
Albert <albertkantwi@gmail.com>
2025-12-06 23:55:29 +0000

    Add product options

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,43 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+  
+const productSelectEl = document.getElementById('product');
+if (productSelectEl) {
+    for (const product of products){ 
+        optionEl = document.createElement("option");
+        optionEl.textContent = product.name; 
+        optionEl.value = product.id;
+
+
+        productSelectEl.appendChild(optionEl); 
+    }
+}
